c974ec1733cab7c35b1cedb9abe9d72613d5e11d
carlosOpenges <carlos@openges.com>
2021-02-16 12:51:01 +0100

    added css loader and dev server

---
+++ starter-kit-babel-html-css-server/src/index.js
@@ -0,0 +1,7 @@
+import style from './style.css';
+
+const arr = [1, 2, 3];
+const codeES6 = () => console.log(...arr);
+codeES6();
+
+//console.log('Hello world');
---
+++ starter-kit-babel-html-css-server/webpack.config.js
@@ -0,0 +1,47 @@
+const { Module } = require("webpack");
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    // entry: {},
+    // output: {},
+    module: {
+        rules: [
+            {
+                test: /\.js$/i,
+                exclude: [/node_modules/, /dist/],
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.html$/i,
+                use: [
+                    {
+                        loader: 'html-loader',
+                        options: { minimize: true }
+                    }
+                ]
+            },
+            {
+                test: /\.css$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader'
+                ]
+            }
+            // To minimize css use CssMinimizerWebpackPlugin
+            // https://webpack.js.org/plugins/css-minimizer-webpack-plugin/
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: './src/template.html',
+            filename: './index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+            chunkFilename: '[id].css'
+        })
+    ]
+}
